2457ab7a7e9ae8b2333d258be4a979f55067fa12
SHIVAM SAHIL <activity.schoolsh2@gmail.com>
2020-05-09 10:37:20 +0530

    Second Commit Adding characters

--- PlayGame.py
+++ PlayGame.py
@@ -3,3 +3,8 @@ from pygame.locals import *
 from pygame.transform import rotate,scale
 from os.path import join
 from random import randint as ri
+
+#Game variables
+ScreenSize=480
+
+screen = pygame.display.set_mode([ScreenSize,ScreenSize])
